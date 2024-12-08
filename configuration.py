842646598793da7ff3cf4276d6eb2c7148e34f41
Evgen1679 <oven.evgeniy@yandex.ru>
2024-12-08 21:56:44 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,7 @@
+# Уксусов Евгений, 24А-я когорта — Инженер по тестированию плюс. Финальный проект.
+
+URL_SERVICE = "https://81093405-3a2e-4602-9014-1006ea9678e6.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+CREAT_NEW_ORDERS = "/api/v1/orders"
